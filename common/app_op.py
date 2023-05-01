46dc6503c82c0b3e997fbde9e0114aabf6e3263e
jayfeather9 <jayfeather9@qq.com>
2023-05-01 22:41:51 +0800

    fix

--- common/app_op.py
+++ common/app_op.py
@@ -20,7 +20,7 @@ class AppOp :
             self.by = by_dict[self.type]
 
     def __call__(self, driver, keyword=None):
-        func_dict = {'try_click': self.click, 'skip': self.skip, 'back': self.back, 'enter': self.enter, 'click': self.click, 'send_keys': self.send_keys, 'find': self.find, 'find_cart_and_click': self.find_cart_and_click}
+        func_dict = {'try_click': self.try_click, 'skip': self.skip, 'back': self.back, 'enter': self.enter, 'click': self.click, 'send_keys': self.send_keys, 'find': self.find, 'find_cart_and_click': self.find_cart_and_click}
         if self.op not in func_dict:
             raise Exception('AppOp run failed op not in skip, back, enter, click, send_keys, find, find_cart_and_click')
         if self.op == 'send_keys':
@@ -74,11 +74,11 @@ class AppOp :
 
     def get_element(self, driver):
         if 'index' in self.op_dict:
-            return self.wait(driver, lambda d: d.find_elements(self.by, (self.value)), self.wait_time)[self.index]
-        return self.wait(driver, lambda d: d.find_element(self.by, (self.value)), self.wait_time)
+            return self.wait(driver, lambda d: d.find_elements(self.by, (self.val)), self.wait_time)[self.index]
+        return self.wait(driver, lambda d: d.find_element(self.by, (self.val)), self.wait_time)
     
     def get_elements(self, driver):
-        return self.wait(driver, lambda d: d.find_elements(self.by, (self.value)), self.wait_time)
+        return self.wait(driver, lambda d: d.find_elements(self.by, (self.val)), self.wait_time)
     
     def wait(self, driver, func, time):
         res = WebDriverWait(driver, timeout=time).until(func)
