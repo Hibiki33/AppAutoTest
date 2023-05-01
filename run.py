4ec9b1c58651d46dcf60381bb98657563efe7dd0
Hibiki33 <113701757+Hibiki33@users.noreply.github.com>
2023-05-01 21:08:42 +0800

    glyx

--- run.py
+++ run.py
@@ -5,8 +5,5 @@ import unittest
 if __name__ == '__init__':
     bilibili = App('caps_bili.json')
     bilibili.app_replace_install()
-
-
-
-
-    bilibili.app_replace_install()
+    unittest.main()
+    bilibili.uninstall()
