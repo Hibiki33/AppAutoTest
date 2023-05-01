d00fd6bb9500047e3d46bb6d5ad0bbd277854831
jayfeather9 <jayfeather9@qq.com>
2023-05-01 20:36:27 +0800

    run appium

---
+++ common/run_appium.py
@@ -0,0 +1,9 @@
+import os
+import _thread
+
+class RunAppium():
+    def __init__(self):
+        _thread.start_new_thread(self.run_appium, ())
+    
+    def run_appium(self):
+        os.system('appium -a localhost -p 4723')
