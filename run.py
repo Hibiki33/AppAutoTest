e1e8faa695ff2ed0dcdeb2d150e9f4ddaa3223a9
jayfeather9 <jayfeather9@qq.com>
2023-05-01 21:23:21 +0800

    run

--- run.py
+++ run.py
@@ -1,9 +1,23 @@
-from app.app import App
-from testcases import test_buy, test_video
+# -*- coding:utf-8 -*-
 import unittest
+import os
+import time
+import HTMLTestRunner
 
-if __name__ == '__init__':
+from app.app import App
+
+# 用例路径
+case_path = os.path.join(os.getcwd(), 'testcases')
+# 报告存放路径
+report_path = os.path.join(os.getcwd(), 'report')
+
+if __name__ == '__main__':
     bilibili = App('caps_bili.json')
     bilibili.app_replace_install()
-    unittest.main()
+    load_case = unittest.defaultTestLoader.discover(case_path, "test_*.py")
+    now = time.strftime("%Y-%m-%d-%H_%M_%S", time.localtime(time.time()))
+    report_abspath = os.path.join(report_path, "result_"+now+".html")
+    with open(report_abspath, "wb") as fp:
+        runner = HTMLTestRunner.HTMLTestRunner(stream=fp, title='App Auto Test', description='Test Results:')
+        runner.run(load_case)
     bilibili.app_uninstall()
