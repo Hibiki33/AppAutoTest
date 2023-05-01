070e197a0e52fe879120477d0178589760b0d78e
jayfeather9 <jayfeather9@qq.com>
2023-05-01 21:35:28 +0800

    kill

--- common/run_appium.py
+++ common/run_appium.py
@@ -6,4 +6,5 @@ class RunAppium():
         _thread.start_new_thread(self.run_appium, ())
     
     def run_appium(self):
+        os.system('adb kill-server')
         os.system('appium -a localhost -p 4723')
