2d861d1bd095d644d6928d581ab9227bce150230
Hibiki33 <113701757+Hibiki33@users.noreply.github.com>
2023-05-03 11:50:35 +0800

    rm

--- test.py
+++
@@ -1,8 +0,0 @@
-from app.app import App
-
-def test_install_func(app_file_json_data):
-    a = App(app_file_json_data)
-    a.app_replace_install()
-
-if __name__ == '__main__':
-    test_install_func('caps_bili.json')
