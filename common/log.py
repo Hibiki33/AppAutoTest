79d2b413d152e6e492263822cdb4491f04593030
BUAA-WFY <42094053+jayfeather9@users.noreply.github.com>
2023-05-03 11:45:25 +0800

    Update log.py

--- common/log.py
+++ common/log.py
@@ -1,8 +1,15 @@
 def log(s, target=None):
     print()
+    try:
+        res = s[0]
+        for i in s:
+            if i != 0:
+                res = i
+                break
     s = str(s)
     if target:
         with open(target, 'w', encoding = 'utf-8') as fp:
             fp.write(s)
     else:
         print(s)
+    return res
